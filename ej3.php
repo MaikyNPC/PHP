cb84a052aeec4d5c477b2a5930aa0ab5a5af0585
MaikyNPC <125688946+MaikyNPC@users.noreply.github.com>
2023-04-26 16:31:35 -0500

    Delete ej3.php

--- ej3.php
+++
@@ -1,8 +0,0 @@
-<?php
-$velocidad = readline("Introduce la velocidad en Km/h: ");
-$tiempo = readline("Introduce el tiempo en horas: ");
-
-$distancia = $velocidad * $tiempo;
-
-echo "La distancia recorrida es: $distancia Km";
-?>
