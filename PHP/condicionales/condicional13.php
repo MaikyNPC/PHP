b7b8be67a76f7a32e0f8c163db3a7d36953e94d8
MaikyNPC <125688946+MaikyNPC@users.noreply.github.com>
2023-04-26 16:31:07 -0500

    Add files via upload

---
+++ PHP/condicionales/condicional13.php
@@ -0,0 +1,28 @@
+﻿<?php
+
+$altura = readline("Ingrese su altura en metros: ");
+$peso = readline("Ingrese su peso en Kg: ");
+
+
+$Imc = $peso / ($altura * $altura);
+
+  if ($Imc < 18.5) {
+    echo "Su IMC es $Imc y está Desnutrido\n";
+  } 
+  elseif ($Imc >= 18.5 && $imc < 25) {
+    echo "Su IMC es $Imc y está Normal\n";
+  } 
+  elseif ($Imc >= 25 && $imc < 30) {
+    echo "Su IMC es $Imc y tiene Sobrepeso\n";
+  } 
+  elseif ($Imc >= 30 && $imc < 35) {
+    echo "Su IMC es $Imc y tiene Obesidad Grado 1\n";
+  } 
+  elseif ($Imc >= 35 && $imc < 40) {
+    echo "Su IMC es $Imc y tiene Obesidad Grado 2\n";
+  } 
+  else {
+    echo "Su IMC es $Imc y tiene Obesidad Grado 3\n";
+  }
+
+?>
