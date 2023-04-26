8c51040ab4053cd676bb20880d220b22308a5b3e
MaikyNPC <125688946+MaikyNPC@users.noreply.github.com>
2023-04-26 16:31:42 -0500

    Delete ej4.php

--- ej4.php
+++
@@ -1,8 +0,0 @@
-<?php
-$anioActual = date('Y'); // Obtener el año actual
-$anioNacimiento = readline("Introduce tu año de nacimiento: "); // Solicitar el año de nacimiento al usuario
-
-$edad = $anioActual - $anioNacimiento; // Calcular la edad
-
-echo "Tu edad es: $edad años";
-?>
