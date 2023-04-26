94f6109aecc534a479576cdc52c73ea74fd458f7
MaikyNPC <125688946+MaikyNPC@users.noreply.github.com>
2023-04-26 16:32:13 -0500

    Delete ej7.php

--- ej7.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-$lado = floatval(readline("Ingrese la longitud del lado del cuadrado en metros: "));
-
-$area = $lado * $lado;
-
-echo "El área del cuadrado es: " . $area . " metros cuadrados.\n";
-?>
