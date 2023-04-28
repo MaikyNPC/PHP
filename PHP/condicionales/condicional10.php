1f3b6b7050e452e921bc357143f6e0e7d1bc1099
MaikyNPC <125688946+MaikyNPC@users.noreply.github.com>
2023-04-28 16:08:03 -0500

    Update condicional10.php

--- PHP/condicionales/condicional10.php
+++ PHP/condicionales/condicional10.php
@@ -1,12 +1,10 @@
 ﻿<?php
-// Leemos el número de llantas de la compra
 echo "Ingrese el número de llantas: ";
 $num_llantas = readline();
 
-// Convertimos la entrada a un número
+
 $num_llantas = intval($num_llantas);
 
-// Calculamos el precio unitario de la llanta
 if ($num_llantas < 6) {
   $precio_unitario = 240000;
 } elseif ($num_llantas < 8) {
@@ -15,10 +13,8 @@ if ($num_llantas < 6) {
   $precio_unitario = 180000;
 }
 
-// Calculamos el precio total de la compra
 $precio_total = $num_llantas * $precio_unitario;
 
-// Imprimimos el precio total de la compra
 echo "El precio total de la compra es: $" . number_format($precio_total) . "\n";
 ?>
 
